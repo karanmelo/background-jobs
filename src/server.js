445e997929faf0fbd8c39f7855c7bf70d74b29ce
karanmelo <karan_melo@hotmail.com>
2021-05-28 10:33:59 -0300

    Implementação do Bull Dashboard

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
 import 'dotenv/config';
 import express from 'express';
+import { createBullBoard } from 'bull-board';
+import { BullAdapter } from 'bull-board/bullAdapter';
+import Queue from './app/lib/Queue';
 
 import UserController from './app/controllers/UserController';
 
 const app = express();
+const { router } = createBullBoard(
+  Queue.queues.map(queue => new BullAdapter(queue.bull))
+);
 
 app.use(express.json());
 
 app.post('/users', UserController.store);
+app.use('/admin/queue', router);
 
 app.listen(3333, () => {
   console.log('Server running on localhost:3333')
